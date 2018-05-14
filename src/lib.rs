e5b878b53b8e0cc25f453c6906115898cb3aa3a5
David Tolnay <dtolnay@gmail.com>
2018-05-14 12:09:24 -0700

    Release 0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -304,7 +304,7 @@
 //! simple reflection-like `reflect::Value` object manipulations into a fully
 //! general and robust procedural macro.
 
-#![doc(html_root_url = "https://docs.rs/reflect/0.0.1")]
+#![doc(html_root_url = "https://docs.rs/reflect/0.0.2")]
 #![allow(unused_variables, dead_code)]
 #![cfg_attr(
     feature = "cargo-clippy",
