2a14a42540854224669289ca36d5e24058183847
Evgenii Ponomarev <eupn@protonmail.com>
2019-11-06 14:34:03 +0800

    Implement field attributes

--- tests/test_field_access.rs
+++ tests/test_field_access.rs
@@ -2,13 +2,25 @@ use quote::quote;
 use reflect::*;
 
 fn derive(ex: Execution) {
+    // Checks from #[trivial(skip)] attribute on a field
+    fn should_skip(field: &Field<Value>) -> bool {
+        field.get_attrs().iter().any(|attr| {
+            syn::custom_keyword!(skip);
+            attr.path.is_ident("trivial") && attr.parse_args::<skip>().is_ok()
+        })
+    }
+
     ex.make_trait_impl(RUNTIME::base::Trait, ex.target_type(), |block| {
         block.make_function(RUNTIME::base::Trait::trivial, |make_function| {
             let receiver = make_function.arg(0);
             match receiver.data() {
                 Data::Struct(Struct::Struct(receiver)) => {
                     for field in receiver.fields() {
-                        field.get_value().get_type_name();
+                        if should_skip(&field) {
+                            continue;
+                        }
+
+                        RUNTIME::base::FieldAccessor::access_field.INVOKE(field.get_value());
                     }
                 }
                 _ => unimplemented!(),
@@ -20,6 +32,11 @@ fn derive(ex: Execution) {
 
 library! {
     use base {
+        type FieldAccessor;
+        impl FieldAccessor {
+            fn access_field(&str);
+        }
+
         trait Trait {
             fn trivial(&self);
         }
@@ -31,15 +48,21 @@ fn test_field_access() {
     let input = quote! {
         struct Test {
             pub test: String,
+
+            #[trivial(skip)]
+            pub skip_me: String,
         }
     };
 
+    // skip_me field should be skipped from the accessing
     let expected = quote! {
-        impl ::base::Trait for Test
-        {
+        impl ::base::Trait for Test {
             fn trivial(&self) {
-                let __v3 = ();
-                __v3
+                let __v0 = self;
+                let __v1 = &__v0.test;
+                let _ = ::base::FieldAccessor::access_field(__v1);
+                let __v4 = ();
+                __v4
             }
         }
     };
