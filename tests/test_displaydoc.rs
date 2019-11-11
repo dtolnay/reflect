9a47576152adb5a32fcebacecb6c2f641e2ae764
David Tolnay <dtolnay@gmail.com>
2019-11-10 18:55:43 -0800

    Move displaydoc derive into integration test file
    The Debug derive gets its own module to clearly demonstrate how the
    snippets in the readme map to real code. The Display derive is just
    there to test macro invocations; to the extent that it serves as
    documentation, the relevant parts should make their way into doc
    comments of the main reflect library.

--- tests/test_displaydoc.rs
+++ tests/test_displaydoc.rs
@@ -1,10 +1,97 @@
+// This test implements a Display derive resembling the displaydoc crate:
+// https://github.com/yaahc/displaydoc
+
 use quote::quote;
+use reflect::*;
+use syn::Meta;
 
-mod display;
+reflect::library! {
+    extern crate std {
+        mod fmt {
+            type Formatter;
+            type Result;
 
-/// This test mimics the [displaydoc] basic crate functionality.
-///
-/// [displaydoc]: https://github.com/yaahc/displaydoc
+            trait Display {
+                fn fmt(&self, &mut Formatter) -> Result;
+            }
+        }
+
+        macro write;
+    }
+}
+
+fn derive_displaydoc(ex: Execution) {
+    ex.make_trait_impl(RUNTIME::std::fmt::Display, ex.target_type(), |block| {
+        block.make_function(RUNTIME::std::fmt::Display::fmt, display_fmt);
+    });
+}
+
+fn display_fmt(f: MakeFunction) -> Value {
+    let receiver = f.arg(0);
+    let formatter = f.arg(1);
+
+    let type_name = receiver.get_type_name();
+    match receiver.data() {
+        Data::Struct(receiver) => match receiver {
+            Struct::Unit(_receiver) => unimplemented!(),
+            Struct::Tuple(_receiver) => unimplemented!(),
+            Struct::Struct(receiver) => {
+                let num_fields = receiver.fields().count();
+                let attrs = receiver.attrs();
+                let doc_comment = extract_doc_comment(attrs);
+                let fmt_string = format!("{} ", doc_comment.unwrap_or("{}".to_owned()));
+
+                let mut last_write = if !fmt_string.contains("{") {
+                    RUNTIME::std::write.INVOKE(&[formatter, f.string(&fmt_string)])
+                } else {
+                    RUNTIME::std::write.INVOKE(&[formatter, f.string(&fmt_string), type_name])
+                };
+
+                for (i, field) in receiver.fields().enumerate() {
+                    let doc_comment = extract_doc_comment(field.get_attrs());
+                    let mut fmt_string = doc_comment.unwrap_or("{}".to_owned());
+
+                    // Add space after field display string, except for the last one
+                    if i + 1 != num_fields {
+                        fmt_string = format!("{} ", fmt_string);
+                    }
+
+                    last_write = RUNTIME::std::write.INVOKE(&[
+                        formatter,
+                        f.string(&fmt_string),
+                        field.get_value(),
+                    ])
+                }
+
+                last_write
+            }
+        },
+        Data::Enum(receiver) => receiver.match_variant(|variant| match variant {
+            Variant::Unit(_variant) => unimplemented!(),
+            Variant::Tuple(_variant) => unimplemented!(),
+            Variant::Struct(_variant) => unimplemented!(),
+        }),
+    }
+}
+
+fn extract_doc_comment(attrs: &[syn::Attribute]) -> Option<String> {
+    for attr in attrs {
+        if attr.path.is_ident("doc") {
+            if let Ok(meta) = attr.parse_meta() {
+                if let Meta::NameValue(syn::MetaNameValue {
+                    lit: syn::Lit::Str(lit),
+                    ..
+                }) = meta
+                {
+                    return Some(lit.value().trim().to_owned());
+                }
+            }
+        }
+    }
+
+    None
+}
+
 #[test]
 fn test_displaydoc() {
     let input = quote! {
@@ -33,6 +120,6 @@ fn test_displaydoc() {
         }
     };
 
-    let actual = reflect::derive(input, display::derive);
+    let actual = reflect::derive(input, derive_displaydoc);
     assert_eq!(actual.to_string(), expected.to_string());
 }
