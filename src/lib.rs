cf0b824709cb9a0c8bac1d0c8918c80d0ec81fd7
Asbjørn Gaarde <asbjorn_gaarde@hotmail.com>
2020-05-13 17:09:09 +0200

    Release 0.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -317,7 +317,7 @@
 //! simple reflection-like `reflect::Value` object manipulations into a fully
 //! general and robust procedural macro.
 
-#![doc(html_root_url = "https://docs.rs/reflect/0.0.4")]
+#![doc(html_root_url = "https://docs.rs/reflect/0.0.5")]
 #![warn(clippy::explicit_iter_loop, clippy::doc_markdown)]
 #![allow(unused_variables, dead_code)]
 #![allow(
